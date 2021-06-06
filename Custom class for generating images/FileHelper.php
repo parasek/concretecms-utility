1c23c8735caf0cc6100cb40974359463f3fa1271
parasek <slawek@radomski.net.pl>
2021-06-06 12:42:30 +0200

    Initial commit

---
+++ Custom class for generating images/FileHelper.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Application\Helpers;
+
+use Concrete\Core\Application\Application;
+use Concrete\Core\Support\Facade\Application as ApplicationFacade;
+use Concrete\Core\Entity\File\Version;
+use Concrete\Core\Entity\File\File as FileEntity;
+use Concrete\Core\File\File;
+use Concrete\Core\File\Set\Set as FileSet;
+use Concrete\Core\File\FileList;
+
+
+/**
+ * Opinionated file-related helpers for concrete5.8 and php7.4+.
+ *
+ * @license https://opensource.org/licenses/MIT The MIT License
+ * @link https://github.com/parasek/c5-snippets
+ */
+class FileHelper
+{
+    protected Application $app;
+
+    public function __construct()
+    {
+        $this->app = ApplicationFacade::getFacadeApplication();
+    }
+
+    /**
+     * Get file info as an array.
+     *
+     * @param $file "File Object / File ID"
+     * @return array
+     */
+    public function getFile($file): array
+    {
+        /* @var Version $file */
+
+        $output = [];
+
+        $file = $this->convertToObject($file);
+        if ($file === null) return $output;
+
+        $output = [
+            'name'          => $this->getModifiedName($file),
+            'file_name'     => $file->getFileName(),
+            'url'           => $file->getURL(),
+            'relative_path' => $file->getRelativePath(),
+            'download_url'  => (string)$file->getForceDownloadURL(),
+        ];
+
+        return $output;
+    }
+
+    /**
+     * Get list of files by file set.
+     *
+     * @param $fileSet "File set Object / File set ID / File set name"
+     * @return array
+     */
+    public function getFilesByFileSet($fileSet): array
+    {
+        $output = [];
+
+        $files = $this->getFilesFromFileset($fileSet);
+
+        foreach ($files as $file) {
+            $output[] = $this->getFile($file);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get a list of files using selected main file.
+     * Files will be fetched from the first found file set.
+     * Selected file will be the first element of list, other files will be sorted by position in file set.
+     *
+     * @param $file "File Object / File ID"
+     * @return array
+     */
+    public function getFilesByMainFile($file): array
+    {
+        $output = [];
+
+        $fileSetFiles = $this->getFilesFromFirstFileSet($file);
+
+        foreach ($fileSetFiles as $fileSetFile) {
+            $output[] = $this->getFile($fileSetFile);
+        }
+
+        return $output;
+    }
+
+    protected function getMainFileset($file): ?FileSet
+    {
+        /* @var FileEntity $file */
+
+        $file = $this->convertToObject($file);
+        if ($file === null) return null;
+
+        return $file->getFileSets()[0];
+    }
+
+    protected function getFilesFromFileset($fileSet): ?array
+    {
+        $output = [];
+
+        if (is_int($fileSet)) {
+            $fileSet = $fileSet = FileSet::getByID($fileSet);
+        }
+
+        if (is_string($fileSet)) {
+            $fileSet = FileSet::getByName($fileSet);
+        }
+
+        if (!is_object($fileSet)) return $output;
+
+        $list = new FileList();
+        $list->filterBySet($fileSet);
+        $list->sortByFileSetDisplayOrder();
+        $output = $list->getResults();
+
+        if (!count($output)) return $output;
+
+        return $output;
+    }
+
+    protected function getFilesFromFirstFileSet($file): array
+    {
+        $output = [];
+
+        $file = $this->convertToObject($file);
+        if ($file === null) return $output;
+
+        $output[0] = $file; // Put main file at the beginning of array
+
+        $mainFileSet = $this->getMainFileset($file);
+
+        if (is_object($mainFileSet)) {
+
+            $fileSetFiles = $mainFileSet->getFiles();
+
+            foreach ($fileSetFiles as $fileSetFile) {
+
+                if (!empty($fileSetFile) and $fileSetFile !== $file) {
+                    $output[] = $fileSetFile;
+                }
+
+            }
+
+        }
+
+        return $output;
+    }
+
+    protected function convertToObject($file): ?FileEntity
+    {
+        if (is_int($file)) {
+            $file = File::getByID($file);
+        }
+
+        if (!($file instanceof FileEntity)) return null;
+
+        return $file;
+    }
+
+    protected function getModifiedName(FileEntity $file): string
+    {
+        /* @var Version $file */
+
+        $title = $file->getTitle();
+
+        $supportedExtensions = ['jpg', 'jpeg', 'png', 'pdf'];
+        $extension = strtolower(pathinfo($file->getTitle(), PATHINFO_EXTENSION));
+        if (!empty($extension) and in_array($extension, $supportedExtensions)) {
+            $title = pathinfo($file->getTitle(), PATHINFO_FILENAME); // Remove extension
+            $title = preg_replace('/ - [0-9]*$/', '', $title); // Remove counter (at the end of filename), " - 001" etc.
+        }
+
+        return $title;
+    }
+}
---
+++ Custom class for generating images/ImageHelper.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace Application\Helpers;
+
+use Concrete\Core\Entity\File\File as FileEntity;
+use Concrete\Core\Entity\File\Version;
+use Concrete\Core\Page\Page;
+
+/**
+ * Opinionated image-related helpers for concrete5.8 and php7.4+ (mostly generating images).
+ *
+ * @license https://opensource.org/licenses/MIT The MIT License
+ * @link https://github.com/parasek/c5-snippets
+ */
+class ImageHelper extends FileHelper
+{
+    private $ih;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ih = $this->app->make('helper/image');
+    }
+
+    /**
+     * Get image info as an array.
+     *
+     * @param $file "File Object / File ID"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to replace default alt attribute (modified concrete5 Title attribute)."
+     * @return array
+     */
+    public function getImage($file, int $width, int $height, bool $crop, ?string $alt = null): array
+    {
+        /* @var Version $file */
+
+        $output = [];
+
+        $file = $this->convertToObject($file);
+        if (!$this->isValidImage($file)) return $output;
+
+        $thumbnail = $this->generateThumbnail($file, $width, $height, $crop);
+
+        $output = [
+            'url'         => $thumbnail['url'],
+            'placeholder' => $this->getPlaceholderString($thumbnail['width'], $thumbnail['height']),
+            'width'       => $thumbnail['width'],
+            'height'      => $thumbnail['height'],
+            'alt'         => $alt ?? $this->getModifiedName($file),
+        ];
+
+        return $output;
+    }
+
+    /**
+     * Get list of images by file set.
+     *
+     * @param $fileSet "File set Object / File set ID / File set name"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to set single alt attribute for all images."
+     * @return array
+     */
+    public function getImagesByFileSet($fileSet, int $width, int $height, bool $crop, ?string $alt = null): array
+    {
+        $output = [];
+
+        $files = $this->getFilesFromFileset($fileSet);
+
+        foreach ($files as $file) {
+            $output[] = $this->getImage($file, $width, $height, $crop, $alt);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get a list of images using selected main image.
+     * Images will be fetched from the first found file set.
+     * Selected image will be the first element of list, other images will be sorted by position in file set.
+     *
+     * @param $file "File Object / File ID"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to set single alt attribute for all images."
+     * @return array
+     */
+    public function getImagesByMainImage($file, int $width, int $height, bool $crop, ?string $alt = null): array
+    {
+        $output = [];
+
+        $fileSetFiles = $this->getFilesFromFirstFileSet($file);
+
+        foreach ($fileSetFiles as $fileSetFile) {
+            $output[] = $this->getImage($fileSetFile, $width, $height, $crop, $alt);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get image info as array (additional image is generated for fullscreen lightbox).
+     *
+     * @param $file "File Object / File ID"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to replace default alt attribute (modified concrete5 Title attribute)."
+     * @param string|null $title "Provide string to replace default lightbox title (which is the same as $alt)."
+     * @return array
+     */
+    public function getGalleryImage($file, int $width, int $height, bool $crop, ?string $alt = null, ?string $title = null): array
+    {
+        $output = [];
+
+        $file = $this->convertToObject($file);
+        if (!$this->isValidImage($file)) return $output;
+
+        $thumbnail = $this->generateThumbnail($file, $width, $height, $crop);
+        $fullscreenImage = $this->generateThumbnail($file, 1920, 1080, false);
+
+        $alt = $alt ? $alt : $this->getModifiedName($file);
+        $title = ($title === null) ? $alt : $title;
+
+        $output = [
+            'url'                     => $thumbnail['url'],
+            'placeholder'             => $this->getPlaceholderString($thumbnail['width'], $thumbnail['height']),
+            'width'                   => $thumbnail['width'],
+            'height'                  => $thumbnail['height'],
+            'fullscreen_image_url'    => $fullscreenImage['url'],
+            'fullscreen_image_width'  => $fullscreenImage['width'],
+            'fullscreen_image_height' => $fullscreenImage['height'],
+            'alt'                     => $alt,
+            'title'                   => $title,
+        ];
+
+        return $output;
+    }
+
+    /**
+     * Get list of images by file set.
+     *
+     * @param $fileSet "File set Object / File set ID / File set name"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to set single alt attribute for all images."
+     * @param string|null $title "Provide string to set single lightbox title for all images."
+     * @return array
+     */
+    public function getGalleryByFileSet($fileSet, int $width, int $height, bool $crop, ?string $alt = null, ?string $title = null): array
+    {
+        $output = [];
+
+        $files = $this->getFilesFromFileset($fileSet);
+
+        foreach ($files as $file) {
+            $output[] = $this->getGalleryImage($file, $width, $height, $crop, $alt, $title);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get a list of images using selected main image.
+     * Images will be fetched from the first found file set.
+     * Selected image will be the first element of list, other images will be sorted by position in file set.
+     *
+     * @param $file "File Object / File ID"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param string|null $alt "Provide string to set single alt attribute for all images."
+     * @param string|null $title "Provide string to set single lightbox title for all images."
+     * @return array
+     */
+    public function getGalleryByMainImage($file, int $width, int $height, bool $crop, ?string $alt = null, ?string $title = null): array
+    {
+        $output = [];
+
+        $fileSetFiles = $this->getFilesFromFirstFileSet($file);
+
+        foreach ($fileSetFiles as $fileSetFile) {
+            $output[] = $this->getGalleryImage($fileSetFile, $width, $height, $crop, $alt, $title);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get list of slider images from file set.
+     * Concrete5 file attributes handles: title, subtitle, link, link_external, link_anchor, button_text, right_alignment, new_window
+     *
+     * @param $fileSet "File set Object / File set ID / File set name"
+     * @param int $width
+     * @param int $height
+     * @param bool $crop
+     * @param array $additionalImageWidths "Provide widths in pixels as an array, for example: [360, 480, 740, 960, 1140, 1372].
+     * Heights will be calculated automatically, taking into account proportions of base image.
+     * Additional images (for different breakpoints) will be generated based on those dimensions."
+     * @return array
+     */
+    public function getSliderImages($fileSet, int $width, int $height, bool $crop, array $additionalImageWidths = []): array
+    {
+        /* @var Version $file */
+
+        $output = [];
+
+        $files = $this->getFilesFromFileset($fileSet);
+        if (empty($files)) return $output;
+
+        $dimensions = [];
+        foreach ($additionalImageWidths as $additionalImageWidth) {
+            $dimensions[] = [
+                'width'  => $additionalImageWidth,
+                'height' => (int)round(($height / $width) * $additionalImageWidth)
+            ];
+        }
+
+        foreach ($files as $file) {
+
+            $srcset = '';
+            $i = 0;
+            foreach ($dimensions as $dimension) {
+                $i++;
+                $srcsetUrl = $this->ih->getThumbnail($file, $dimension['width'], $dimension['height'], $crop)->src;
+                $intrinsicWidthInPixels = $dimension['width'];
+                $srcset .= $srcsetUrl . ' ' . $intrinsicWidthInPixels . 'w';
+                if ($i != count($dimensions)) {
+                    $srcset .= ',' . PHP_EOL;
+                }
+            }
+
+            $link = null;
+            if ($linkAttribute = $file->getAttribute('link')) {
+                $linkPage = Page::getByID($linkAttribute);
+                if (is_object($linkPage) and !$linkPage->isError() and !$linkPage->isInTrash()) {
+                    $link = $linkPage->getCollectionLink();
+                }
+            }
+            if ($file->getAttribute('link_external')) {
+                $link = $file->getAttribute('link_external');
+            }
+
+            $output[] = [
+                'url'             => $this->ih->getThumbnail($file, $width, $height, $crop)->src,
+                'width'           => $width,
+                'height'          => $height,
+                'srcset'          => $srcset,
+                'sizes'           => '(max-width: 767px)  100vw,
+                                      (max-width: 991px)  740px,
+                                      (max-width: 1199px) 960px,
+                                      (max-width: 1649px) 1140px,
+                                     ' . $width . 'px',
+                'title'           => $file->getAttribute('title') ?? null,
+                'subtitle'        => $file->getAttribute('subtitle') ?? null,
+                'link'            => $link,
+                'link_anchor'     => $file->getAttribute('link_anchor') ?? null,
+                'button_text'     => $file->getAttribute('button_text') ?? null,
+                'right_alignment' => $file->getAttribute('right_alignment') ?? null,
+                'new_window'      => $file->getAttribute('new_window') ?? null
+            ];
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get an inline svg string that can be used for image "src" attribute when using lazy-loading.
+     *
+     * @param int $width
+     * @param int $height
+     * @return string
+     */
+    public function getPlaceholderString(int $width, int $height): string
+    {
+        return 'data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20' . $width . '%20' . $height . '%22%20%2F%3E';
+    }
+
+    private function generateThumbnail(FileEntity $file, int $width, int $height, bool $crop): array
+    {
+        /* @var Version $file */
+
+        $thumbnailUrl = $file->getURL();
+        $thumbnailWidth = $file->getAttribute('width');
+        $thumbnailHeight = $file->getAttribute('height');
+        if ($thumbnailWidth > $width or $thumbnailHeight > $height) {
+            $thumbnail = $this->ih->getThumbnail($file, $width, $height, $crop);
+            $thumbnailUrl = $thumbnail->src;
+            $thumbnailWidth = $thumbnail->width;
+            $thumbnailHeight = $thumbnail->height;
+        }
+
+        return [
+            'url'    => $thumbnailUrl,
+            'width'  => $thumbnailWidth,
+            'height' => $thumbnailHeight,
+        ];
+    }
+
+    private function isValidImage($file): bool
+    {
+        /* @var Version $file */
+
+        if (!($file instanceof FileEntity)) return false;
+        if (!$file->canEdit()) return false;
+
+        return true;
+    }
+}
